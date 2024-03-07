9a5f66a98359f401a702884f47507f11e139631d
Sebastian Reid <techninjas514@gmail.com>
2024-03-06 22:28:03 -0500

    serve react in express

--- server/app.js
+++ server/app.js
@@ -17,6 +17,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/index.html'));
+})
 
 const Sequelize = require("sequelize");
 const {
