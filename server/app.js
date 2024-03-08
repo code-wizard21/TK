c50f4733f3f77fb6c4048eae374da5d918f3754d
Sebastian Reid <techninjas514@gmail.com>
2024-03-07 21:25:12 -0500

    compression

--- server/app.js
+++ server/app.js
@@ -6,6 +6,7 @@ const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 const pg = require("pg");
 const indexRouter = require("./routes/index");
+const compression = require('compression');
 
 const app = express();
 
@@ -16,6 +17,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression());
 app.use(express.static(path.join(__dirname, "public")));
 
 const Sequelize = require("sequelize");
